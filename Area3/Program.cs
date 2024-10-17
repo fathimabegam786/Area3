525dd9fdb69533d0227fa5c5dda979a34d38adc0
shaik hanifa <hanifashaik285@gmail.com>
2024-10-17 17:06:22 +0530

    Add project files.

---
+++ Area3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape s = new Shape();
+
+            Console.WriteLine("circle area is:" + s.area(5));
+            Console.WriteLine("Rectangle area is:" + s.area(5f, 5f));
+            Console.WriteLine("Triangle area is:" + s.area(7m, 6m));
+
+
+            Console.ReadLine();
+        }
+        public class Shape
+        {
+            public float area(float r)
+            {
+                return 3.14f * r * r;
+            }
+            public float area(float l, float w)
+            {
+                return l * w;
+            }
+            public decimal area(decimal b, decimal h)
+            {
+                return 0.5m * b * h;
+            }
+        }
+    }
+}
+      
